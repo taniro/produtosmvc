050186c15e38854c3281760679337590bb353b2b
Taniro <tanirocr@gmail.com>
2022-05-31 11:33:58 -0300

    aula 31

--- src/main/java/tads/eaj/br/produtosmvc/model/Produto.java
+++ src/main/java/tads/eaj/br/produtosmvc/model/Produto.java
@@ -28,4 +28,36 @@ public class Produto {
     String descricao;
     @Min(0)
     Float preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
 }
